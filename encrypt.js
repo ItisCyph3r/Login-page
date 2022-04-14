430838d480716373e7583da72e0c99d38e53c007
Momoh Samuel <samuelmomoh61@gmail.com>
2022-04-14 23:31:59 +0100

    changed enc key

--- encrypt.js
+++ encrypt.js
@@ -1,19 +1,19 @@
 const crypto = require('crypto');
-const ENC= 'bf3c199c2470cb477d907b1e0917c17b';
-const IV = "5183666c72eec9e4";
+const ENC= 'j1uy3lyq3ul3fg5i21z4ykhg63Hh5l7k';
+const IV = "s9pr4k4h2lj1l163";
 const ALGO = "aes-256-cbc"
 
 exports.encrypt = (text) => {
-   let cipher = crypto.createCipheriv(ALGO, ENC, IV);
-   let encrypted = cipher.update(text, 'utf8', 'base64');
-   encrypted += cipher.final('base64');
-   return encrypted;
+    let cipher = crypto.createCipheriv(ALGO, ENC, IV);
+    let encrypted = cipher.update(text, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
 };
 
 exports.decrypt = (text) => {
-   let decipher = crypto.createDecipheriv(ALGO, ENC, IV);
-   let decrypted = decipher.update(text, 'base64', 'utf8');
-   return (decrypted + decipher.final('utf8'));
+    let decipher = crypto.createDecipheriv(ALGO, ENC, IV);
+    let decrypted = decipher.update(text, 'base64', 'utf8');
+    return (decrypted + decipher.final('utf8'));
 };
 
 // const encrypted_key = encrypt("HelloWorld");
